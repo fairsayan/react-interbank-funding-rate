7e20dc8d821b86f5ac3e9b6decec5231c693fe6b
Domenico Pontari <fairsayan@gmail.com>
2020-10-27 12:06:12 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import cheerio from 'cheerio'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function getDataFromHtml(html) {
+    const $ = cheerio.load(html)
+    const rows = $('table').eq(2).find('tr')
+    const data = []
+    
+    for (let i = 0; i < rows.length; i++) {
+        const row = rows.eq(i)
+        const index = row.find('td').eq(0).text().trim()
+        const interbankFundingRate = row.find('td').eq(1).text().trim()
+        if (!index) continue
+        data.push([index, interbankFundingRate])
+    }
+
+    return data
 }
 
-export default App;
+function Table(props) {
+    const rows = props.data.map( row => (
+        <tr>
+            <td>{ row[0] }</td>
+            <td>{ row[1] }</td>
+        </tr>
+    ) )
+
+    return ( <table> { rows } </table> )
+}
+
+function App() {
+    const [data, setData] = useState(null)
+
+    useEffect( () => {
+        async function fetchData() {
+            try {
+                const proxy = 'https://cors-anywhere.herokuapp.com/' // avoiding CORS
+                const url = 'https://www.oanda.com/rw-en/trading/financing-costs/'
+                const response = await axios.get(`${proxy}${url}`)
+                setData( getDataFromHtml(response.data) )
+            } catch (e) {
+                console.error(e)
+            }
+        }
+        if (!data) fetchData()
+    })
+
+    return (
+        <div className="App">
+            { data ? <Table data={ data } /> : null }
+        </div>
+    )
+}
+
+export default App
